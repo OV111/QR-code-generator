8425173b1549638f4b5f935d6ab7212c4368e111
Vahe <ohanyanv703@gmail.com>
2025-02-25 21:09:28 +0400

    Adding variables in js!

---
+++ index.js
@@ -0,0 +1,31 @@
+const qrType = document.getElementById("qrType");
+const qrInput = document.getElementById("qrInput");
+const imageSize = document.getElementById("imageSize");
+
+const generateBtn = document.getElementById("GenerateBtn");
+const qrContainer = document.getElementById("image");
+const downloadBtn = document.getElementById("DownloadBtn");
+
+
+function generateQR() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateInputField() {
+
+}
+function updateImageSize() {
+
+}
