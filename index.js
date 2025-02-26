892e564813539d63e22c6785b3ec7559c5a8153f
Vahe <ohanyanv703@gmail.com>
2025-02-26 20:10:33 +0400

    generate function js

--- index.js
+++ index.js
@@ -3,19 +3,38 @@ const qrInput = document.getElementById("qrInput");
 const imageSize = document.getElementById("imageSize");
 
 const generateBtn = document.getElementById("GenerateBtn");
-const qrContainer = document.getElementById("image");
+const qrContainer = document.getElementById("QRimage");
 const downloadBtn = document.getElementById("DownloadBtn");
 
 
-function generateQR() {
-    
+
+if(qrType) {
+
 }
 
+const generateQR = () => {
+        const typeData = qrType.value;
+        const inputData = qrInput.value.trim();
+        const imageSizeData = imageSize.value;
+
+       const qrcode = new QRCode(qrContainer,{
+            text: `https://` + inputData,
+            width:imageSizeData,
+            height:imageSize,
+       });
+       console.log(qrcode);
+        // console.log(typeData,inputData,imageSizeData);
+}   
+
+
+generateBtn.addEventListener("click", generateQR);
 
 
 
-
-
+//! change all function with arrow f
+function check() {
+    // check for valid and selected values in input fields!!!
+}
 
 
 
