e6dd20392e0d8f33ebdf1c3453be5cc565caa746
Vahe <ohanyanv703@gmail.com>
2025-02-26 22:33:17 +0400

    adding almost fully html,css

--- index.js
+++ index.js
@@ -8,21 +8,41 @@ const downloadBtn = document.getElementById("DownloadBtn");
 
 
 
-if(qrType) {
-
-}
-
 const generateQR = () => {
-        const typeData = qrType.value;
-        const inputData = qrInput.value.trim();
-        const imageSizeData = imageSize.value;
-
-       const qrcode = new QRCode(qrContainer,{
-            text: `https://` + inputData,
-            width:imageSizeData,
-            height:imageSize,
-       });
-       console.log(qrcode);
+    const typeData = qrType.value;             // the type of qr
+    const inputData = qrInput.value.trim();    // what we write input field
+    const imageSizeData = imageSize.value;     // size of image
+    let data;
+    if(typeData) {                       //! need to think about input validation
+        if(typeData === "url") {
+            data = `https://${inputData}`;             
+        } else if(typeData === "phone") {
+            data = `tel:${inputData}`;
+        } else if(typeData === "email") {
+            data = `mailto:${inputData}`;
+        } else if(typeData === "wifi") {
+            data = `WIFI:T:WPA;S:${inputData}`;
+        } else if(typeData === "gps") {
+            let x = inputData.split(",");
+            if(x.length === 2) {
+                let latitude = x[0];
+                let longitude = x[1];
+                data = `geo: ${latitude},${longitude}`;
+            }
+        } else {
+            alert("Pls enter Valid Type");
+        }
+    }
+    console.log(data)
+    const qrcode = new QRCode(qrContainer,{
+        text: data,
+        width: imageSizeData,
+        height: imageSizeData,
+    });
+    // console.log(qrcode.text)
+    console.log(qrContainer)                               //! need to update the image in case of 
+                                                        //! one more generateBtn 
+    // console.log(qrcode,qrContainer.);
         // console.log(typeData,inputData,imageSizeData);
 }   
 
@@ -31,7 +51,7 @@ generateBtn.addEventListener("click", generateQR);
 
 
 
-//! change all function with arrow f
+//! change all function with arrow f           and add show/hide spinner like loading 
 function check() {
     // check for valid and selected values in input fields!!!
 }
